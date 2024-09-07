c88de4995d4afeb6d80b789faabc27688e63df55
Manish Shivhare <67515371+manishshivhare@users.noreply.github.com>
2024-09-08 01:01:05 +0530

    optimized code

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types'; // Import PropTypes for type-checking
 
 function ProjectCard({ src, link, h3, p }) {
   return (
-    <a href={link} target="_blank">
-      <img className="hover" src={src} alt={`${h3} logo`} />
-      <h3>{h3}</h3>
-      <p>{p}</p>
+    <a href={link} target="_blank" rel="noopener noreferrer" className="projectCard">
+      <div className="projectCardContent">
+        <img className="projectImage hover" src={src} alt={`${h3} logo`} />
+        <h3>{h3}</h3>
+        <p>{p}</p>
+      </div>
     </a>
   );
 }
+
+// Prop types validation
+ProjectCard.propTypes = {
+  src: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  h3: PropTypes.string.isRequired,
+  p: PropTypes.string.isRequired,
+};
 
 export default ProjectCard;
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,32 +1,44 @@
 import styles from './ProjectsStyles.module.css';
 import foa from '../../assets/flipkart-order-analyzer.png';
 import game2048 from '../../assets/2048.png';
+import blogImage from '../../assets/manish-image.png';
 import ProjectCard from '../../common/ProjectCard';
+
+const projects = [
+  {
+    src: foa,
+    link: 'https://chromewebstore.google.com/detail/mcpflafdobpbfojllbpbciphhgknnjje?authuser=0&hl=en-GB',
+    h3: 'Order Analyzer',
+    p: 'Flipkart orders analyzer',
+  },
+  {
+    src: blogImage,
+    link: 'https://blog-manish-shivhare.onrender.com/',
+    h3: 'Blog site',
+    p: 'Personal blog site using MERN',
+  },
+  {
+    src: game2048,
+    link: 'https://manish-shivhare-2048.netlify.app/',
+    h3: '2048-Game',
+    p: 'Famous 2048 Game Clone',
+  },
+];
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <ProjectCard
-          src={foa}
-          link="https://chromewebstore.google.com/detail/mcpflafdobpbfojllbpbciphhgknnjje?authuser=0&hl=en-GB"
-          h3="Order Analyzer"
-          p="Flipkart orders analyzer"
-        />
-        <ProjectCard
-          src='https://manish-shivhare.netlify.app/assets/manish-image-BMnFPNJG.png'
-          link="https://blog-manish-shivhare.onrender.com/"
-          h3="Blog site"
-          p="Personal blog site using MERN"
-        />
-        <ProjectCard
-          src={game2048}
-          link="https://manish-shivhare-2048.netlify.app/"
-          h3="2048-Game"
-          p="Famous 2048 Game Clone"
-        />
-        
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={index}
+            src={project.src}
+            link={project.link}
+            h3={project.h3}
+            p={project.p}
+          />
+        ))}
       </div>
     </section>
   );
