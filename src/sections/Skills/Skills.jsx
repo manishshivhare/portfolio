70fd25c37dbf828b54446068dbcdf2d9f1227f9a
Manish Shivhare <67515371+manishshivhare@users.noreply.github.com>
2024-09-13 13:42:18 +0530

    update skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -16,6 +16,7 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
         <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="RESTful API" />
         
       </div>
       <hr />
